ff1d1e519a225f64570d9d48590245dc33cf568d
Lucas Kelim Thiel <lucas.kthiel@gmail.com>
2025-02-21 15:40:11 -0300

    Creating relations

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,19 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Product;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
-class ProductController extends Controller
+class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
-        $products = Product::all();
-
-        return view("product.index", compact("products"));
+        //
     }
 
     /**
@@ -36,7 +34,7 @@ class ProductController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Product $product)
+    public function show(Customer $customer)
     {
         //
     }
@@ -44,7 +42,7 @@ class ProductController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Product $product)
+    public function edit(Customer $customer)
     {
         //
     }
@@ -52,7 +50,7 @@ class ProductController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Product $product)
+    public function update(Request $request, Customer $customer)
     {
         //
     }
@@ -60,7 +58,7 @@ class ProductController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Product $product)
+    public function destroy(Customer $customer)
     {
         //
     }
---
+++ resources/views/product/index.blade.php
@@ -0,0 +1,35 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Products') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    @forelse ($products as $product)
+                        <div class="flex pb-3 items-center justify-between border-b-2 last:border-b-0 last:pb-0">
+                            <div>
+                                <a href="{{ route('products.show', $product->id) }}">
+                                    <h2 class="text-lg font-semibold">{{ $product->name }}</h2>
+                                </a>
+                                <p class="text-sm text-gray-600">R$ {{ $product->price }}</p>
+                            </div>
+                            <div class="flex space-x-2">
+                                {{-- <a href="{{ route('products.buy', $product->id) }}"> --}}
+                                    <x-primary-button>
+                                        Buy
+                                    </x-primary-button>
+                                {{-- </a> --}}
+                            </div>
+                        </div>
+                    @empty
+                        <h2>Any product found</h2>
+                    @endforelse
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
